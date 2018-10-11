841a2fa2dfedf243d604a545d38cc72167b91bde
JuanRodriguez19 <43156470+JuanRodriguez19@users.noreply.github.com>
2018-10-11 16:10:38 -0400

    Quit Code Working 4:10

--- Lab2/app/src/main/java/com/example/juan/lab2/MainActivity.java
+++ Lab2/app/src/main/java/com/example/juan/lab2/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.juan.lab2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId()==R.id.user){
+            moveTaskToBack(true);
+            android.os.Process.killProcess(android.os.Process.myPid());
+            System.exit(1);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
